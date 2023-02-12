aed3c9ac504803d9751535f62d4f6b61b4efa2c8
Amadou <atidiane.camara@gmail.com>
2023-02-12 14:39:35 +0100

    [students.test.js] add GET /studens test suite

--- __test__/students.test.js
+++ __test__/students.test.js
@@ -21,7 +21,37 @@ afterAll(async () => {
 });
 
 describe('Attendance API Students route', () => {
-  it('it should return true', () => {
-    expect(true).toBeTruthy();
+  describe('GET /students', () => {
+    it('should return a 200 status code', async () => {
+      const res = await request(app).get('/students');
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('should return a nomber of students record', async () => {
+      const res = await request(app).get('/students');
+      expect(res.body).toHaveProperty('count');
+      expect(res.body.count).toBe(2);
+    });
+  });
+
+  it('should return correct data in db', async () => {
+    const res = await request(app).get('/students');
+    expect(res.body.rows[0]).toMatchObject({
+      id: 1,
+      firstname: 'John',
+      lastname: 'DOE',
+      schoolClassId: 1,
+    });
+    expect(res.body.rows[0].createdAt).toContain('2023-02-10T16:09:37');
+    expect(res.body.rows[0].updatedAt).toContain('2023-02-10T16:09:37');
+
+    expect(res.body.rows[1]).toMatchObject({
+      id: 2,
+      firstname: 'Jane',
+      lastname: 'KING',
+      schoolClassId: 3,
+    });
+    expect(res.body.rows[1].createdAt).toContain('2023-02-10T16:09:37');
+    expect(res.body.rows[1].updatedAt).toContain('2023-02-10T16:09:37');
   });
 });
